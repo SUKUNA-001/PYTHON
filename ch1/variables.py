f02b55c56e871e1da5a25962447389a05b583395
SUKUNA-001 <manik.kumar1850@gmail.com>
2024-08-24 10:49:48 +0530

    Create variables.py

---
+++ ch1/variables.py
@@ -0,0 +1,29 @@
+# string
+name = "isha"
+print(type(name))
+#integer
+roll_number = 12
+
+# floating number
+percentage = 98
+
+#boolean
+is_student = True
+
+print(name, roll_number, percentage, is_student)
+percentage = 94
+print(name, roll_number, percentage, is_student)
+print("my name is " + name + " my roll number is",roll_number)
+print("i scored",percentage,"% in my finals. i am a student", is_student)
+
+#percentage = 94
+
+
+#print expressions
+print('my percentage has changed to',percentage - 1)
+
+
+x = 1
+y = 2
+z = 3
+print(x,y,z,sep="-")
