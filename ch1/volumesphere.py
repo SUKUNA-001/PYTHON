df0416e1fc6326ee5f97b5910f8ec8f0578753fa
SUKUNA-001 <manik.kumar1850@gmail.com>
2024-08-24 10:49:53 +0530

    Create volumesphere.py

---
+++ ch1/volumesphere.py
@@ -0,0 +1,8 @@
+#radius of sphere as input
+radius = float(input("enter radius of sphere"))
+
+ # cal vol.
+volume = (4 / 3 )* 3.14 * (radius ** 3)
+
+#print vol.
+print("volume of sphere is:", volume)
