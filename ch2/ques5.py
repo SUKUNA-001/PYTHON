1c8265aa8eade17c05836b77fc4713afdef5d9d6
SUKUNA-001 <manik.kumar1850@gmail.com>
2024-08-24 10:52:10 +0530

    Create ques5.py

---
+++ ch2/ques5.py
@@ -0,0 +1,13 @@
+num = int(input(" enter the positive number:"))
+
+# checking if its divisible by 15
+if num % 15 == 0:
+
+# checking if num id divisble by 3 or 5
+
+
+    if num % 3 == 0 or num % 5 == 0:
+        print("num is not divisible by 15 but is divisible by 3 or 5")
+    else: 
+        print("num is neither divisible by 3 nor by 5")
+
