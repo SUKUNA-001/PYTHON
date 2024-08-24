b4a3279ae848eb376591e8b80c95588fadf14110
SUKUNA-001 <manik.kumar1850@gmail.com>
2024-08-24 10:49:44 +0530

    Create typecasting.py

---
+++ ch1/typecasting.py
@@ -0,0 +1,12 @@
+a = 3 
+b = 4
+c = 5
+
+a_str = str(a)
+b_str = str(b)
+c_str = str(c)
+
+final_string = a_str + b_str + c_str
+final_int = int(final_string)
+print("final_int:", final_int, type(final_int))
+      
