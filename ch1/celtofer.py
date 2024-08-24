9b91d014788b8a5208ab01842bcb724227c64541
SUKUNA-001 <manik.kumar1850@gmail.com>
2024-08-24 10:48:58 +0530

    Create celtofer.py

---
+++ ch1/celtofer.py
@@ -0,0 +1,5 @@
+#taking temp. in celcius to farenheit
+celcius_temp = float(input("enter the temprature in celcius"))
+
+fer_temp = (celcius_temp * 9 / 5) + 32
+print("temperature in farenheit:", fer_temp)
