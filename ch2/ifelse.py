5a548fb7584c4fc38b885fda19866100fcef1920
SUKUNA-001 <manik.kumar1850@gmail.com>
2024-08-24 10:51:21 +0530

    Create ifelse.py

---
+++ ch2/ifelse.py
@@ -0,0 +1,13 @@
+number = int(input("enter a number:"))              
+
+if number >=  0:
+    print("the number is positive")
+
+else:
+    print("the number is negative")
+
+if number % 2 == 0:
+    print("the numberv is even")
+
+else:
+    print("the number is odd")
