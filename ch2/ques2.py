113ef04eeadace348fe36a007b072ef88ac08242
SUKUNA-001 <manik.kumar1850@gmail.com>
2024-08-24 10:51:58 +0530

    Create ques2.py

---
+++ ch2/ques2.py
@@ -0,0 +1,15 @@
+marks = int(input("enter the mark"))
+# if maeks is more than 80
+if marks > 80:
+    print("very good")
+# if marks are between 61 and 80
+elif marks > 60:
+    print("good")
+# if marks are between 41 and 60
+elif marks > 40:
+    print("average")
+# if marks is less than equal to 40
+else: 
+    print("fail")
+
+
