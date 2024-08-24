0128c72a1a6635bb37b5cd323dbf9bd531fe587a
SUKUNA-001 <manik.kumar1850@gmail.com>
2024-08-24 10:49:23 +0530

    Create input.py

---
+++ ch1/input.py
@@ -0,0 +1,9 @@
+#name = input("enter your name: ")
+#print(name)
+
+#age = int(input("enter your age: "))
+#print(type(age))
+number1 = int(input("enter number1: "))
+number2 = int(input("enter number2: "))
+sum = number1 + number2
+print("the sum of these two number is", sum)
