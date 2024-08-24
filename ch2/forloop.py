e3d1eb987db4098385c69b6cb63fef451b048d7c
SUKUNA-001 <manik.kumar1850@gmail.com>
2024-08-24 10:51:25 +0530

    Create forloop.py

---
+++ ch2/forloop.py
@@ -0,0 +1,13 @@
+#for i in range(1,11):
+   # print(i, "helo print")
+#for i in range(1,11,2): # dont have to use i just left blank
+   # print(i, "hlo world")
+
+    # print elements of a list using forloop
+list = [10,20,30,40,50]
+for _ in list:
+        print(_)
+
+input = ("hlo world")
+for i in range(1,10):
+    print(i, input)
