6a7338324adf2c820580102651a465c5b389f9e0
SUKUNA-001 <manik.kumar1850@gmail.com>
2024-08-25 15:04:57 +0530

    Create fibonaccisq.py

---
+++ ch4 and ch5/fibonaccisq.py
@@ -0,0 +1,13 @@
+def fibonacci(n):
+
+    #base case
+    if n ==1:
+        return 0
+    elif n == 2:
+        return 1
+    else:
+        return(fibonacci(n-1) + fibonacci(n-2)) #recursive cse
+    
+n = int(input("enter n:"))
+for i in range(1, n+1):
+    print(fibonacci(i))
