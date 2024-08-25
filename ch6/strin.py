1cc27bd5586df59b7a133ad12784037abf85faca
SUKUNA-001 <manik.kumar1850@gmail.com>
2024-08-25 15:05:40 +0530

    Create strin.py

---
+++ ch6/strin.py
@@ -0,0 +1,38 @@
+name1 = 'manik goat'
+
+name2 = "manik goat"
+
+name3 = '''manik
+goat'''
+
+#print(name1,name2,name3)
+#print(type(name3))
+#print(type(name2))
+#print(type(name1))
+
+#indexing a string
+#print(name1[0])
+#print(name2[3])
+
+#transversing a string
+#FORLOOP
+#for i in name1:
+ #   print(i)
+
+#USING LIST COMPREHENSION
+#list = [char for char in name1]
+#for i in list:
+ #   print(i)
+
+#LENGTH OF A STRING
+
+#print(len(name1))
+
+#fing a char/substring   in string
+#print(name3.find(' '))
+
+#SLICING STRING
+# M A N I K  G O A T
+# 0 1 2 3 4 5 6 7 8 9 
+
+#print(name1[4:9])
