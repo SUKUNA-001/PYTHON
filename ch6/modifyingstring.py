4d954600c23e3d3b978e056fb61de17aa3a1c064
SUKUNA-001 <manik.kumar1850@gmail.com>
2024-08-25 15:05:29 +0530

    Create modifyingstring.py

---
+++ ch6/modifyingstring.py
@@ -0,0 +1,49 @@
+# for converting character to uppercase 
+#str1 = "new york"
+#str2 = str1.upper()
+#print(str2)
+
+# for converting character to lower case
+#str3 = str2.lower()
+#print(str3)
+
+#capitalising the first charactr of my string
+#str4 = str3.capitalize()
+#print(str4)
+
+
+#for stripping/removing any trailing whitespace
+#str1 = "     hello world    "
+#print(str1.strip())
+
+#REPLACE SUBSTRING
+#str1 = "hello world, what a beautiful world this is "
+#print(str1.replace("world", "earth", 1))
+
+#SPLITTING STRING
+#str1 = "ria pia sia dia mia"
+#list = str1.split()
+#print(list)
+
+#CONCATINATE
+#str1 = "hello world"
+#str2 = "what a beautiful day"
+#print(str1 + str2)
+
+#STRING FORMATTING
+#student_name = "zxcvbnm"
+#marks = "99"
+
+#str1 = "the studen_name is {s} , marks is {m}".format(s = student_name, m = marks)
+#print (str1)
+
+
+text = "There is no accidents "
+print(text)
+print(len(text))
+print(text.find('cc'))
+print(text[:9])
+print(text.replace('is' , 'are'))
+text2 = "only ur fate"
+new_text = text + text2
+print(new_text)
